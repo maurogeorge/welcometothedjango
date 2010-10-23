c39271ea4999e64d3cde95d9aaca3ddbba78342b
maurogeorge <maurogot@gmail.com>
2010-10-23 08:53:03 -0200

    Alterando a view que não rodou no herike

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
-from django.template import RequestContext
+
 
 def homepage(request):
-    context = RequestContext(request)
+    from django.conf import settings
+    context = {'MEDIA_URL': settings.MEDIA_URL}
+
     return render_to_response('index.html', context)
